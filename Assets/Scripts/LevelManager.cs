e12564ce8204b21d0f089a8f07f87cc3047ad105
stephencarter-io <stephen@stephencarter.io>
2017-12-31 08:56:39 +1000

    Finish lecture 131

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	private void Start() {
+		Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+	}
+
+	public void LoadLevel(string name) {
+		Debug.Log ("Level load requested for: " + name);
+		SceneManager.LoadScene (name);
+	}
+
+	public void QuitRequest() {
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+	public void LoadNextLevel() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
